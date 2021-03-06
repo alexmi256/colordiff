96a73db8361a4c31808ba3126d1273c075d18104
Alex Mi <alexmi3.14@gmail.com>
2021-03-06 12:23:37 -0500

    add sample dbscan

--- src/example.py
+++ src/example.py
@@ -72,7 +72,8 @@ distance_matrix = make_matrix()
 distances = pairwise_distances(distance_matrix, metric="precomputed")
 
 # Compute DBSCAN
-db = DBSCAN(eps=10, metric="precomputed", min_samples=1).fit(distances)
+# The results are pretty bad
+db = DBSCAN(eps=20, metric="precomputed", min_samples=1).fit(distances)
 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
 core_samples_mask[db.core_sample_indices_] = True
 labels = db.labels_
@@ -83,4 +84,26 @@ n_noise_ = list(labels).count(-1)
 
 print("Estimated number of clusters: %d" % n_clusters_)
 print("Estimated number of noise points: %d" % n_noise_)
+
+# #############################################################################
+# Plot result
+
+# Black removed and is used for noise instead.
+unique_labels = set(labels)
+colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
+for k, col in zip(unique_labels, colors):
+    if k == -1:
+        # Black used for noise.
+        col = [0, 0, 0, 1]
+
+    class_member_mask = labels == k
+
+    xy = distances[class_member_mask & core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1], "o", markerfacecolor=tuple(col), markeredgecolor="k", markersize=14)
+
+    xy = distances[class_member_mask & ~core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1], "o", markerfacecolor=tuple(col), markeredgecolor="k", markersize=6)
+
+plt.title("Estimated number of clusters: %d" % n_clusters_)
+plt.show()
 
