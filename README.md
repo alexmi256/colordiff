ad2ffedf8aa2012feecd65b327745f7e8d42ddcb
Alex Mi <alexmi3.14@gmail.com>
2021-03-06 11:20:02 -0500

    add color similarity matrix

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # colordiff
-A toy project to test out clustering different colors using visual similarity
+A toy project to test out clustering using the visual similarity of colors
+
+The point of this project is to be able to test out different clustering algorithms and parameters using the similarity (or distance between) two colors.
+
+## Color Choices
+To keep things simple we're randomly choosing some [terminal colors](https://jonasjacek.github.io/colors/).
+
+## Color Space
+The colors are stored in RGB, in order to be able to compare them we convert them to the [CIELAB color space](https://en.wikipedia.org/wiki/CIELAB_color_space)
+
+## Color Difference
+Once colors are converted we can compare their similarity using the [CIEDE2000 color difference formula.](https://en.wikipedia.org/wiki/Color_difference#CIEDE2000)
+The color difference (aka Delta-E) seems to have a range of 0-100+ where 1.0 is the smallest color difference the human eye can see.
+Unfortunately I could not find any sources explaining this more besides [this](http://zschuessler.github.io/DeltaE/learn/) and [this](http://www.colorwiki.com/wiki/Delta_E:_The_Color_Difference)
+Which has the chart:
+
+| Delta-E | Description                            |
+|---------|----------------------------------------|
+| <= 1.0  | Not perceptible by human eyes.         |
+| 1 - 2   | Perceptible through close observation. |
+| 2 - 10  | Perceptible at a glance.               |
+| 11 - 49 | Colors are more similar than opposite  |
+| 100     | Colors are exact opposite              |
+
+## Similarity Matrix
+Here's the terminal output of the similarity matrix
+![15x15 Color Similarity Matrix using CIEDE2000](images/color_similarity_matrix.png "15x15 Color Similarity Matrix using CIEDE2000")
---
+++ src/example.py
@@ -0,0 +1,59 @@
+from colormath.color_objects import LabColor, sRGBColor
+from colormath.color_diff import delta_e_cie2000
+from colormath.color_conversions import convert_color
+from colored.hex import _xterm_colors
+import matplotlib.pyplot as plt
+import random
+from rich.console import Console
+from rich.table import Table
+from rich.text import Text
+
+NUMBER_OF_COLORS = 15
+
+# Choose some random color
+all_colors_hex = list(_xterm_colors.values())
+chosen_colors_hex = random.sample(all_colors_hex, NUMBER_OF_COLORS)
+chosen_colors_rgb = [sRGBColor.new_from_rgb_hex(x) for x in chosen_colors_hex]
+chosen_colors_lab = [convert_color(x, LabColor) for x in chosen_colors_rgb]
+
+# Generate a similarity matrix of all colors
+matrix = []
+for color_1 in chosen_colors_lab:
+    row = [delta_e_cie2000(color_1, color_2) for color_2 in chosen_colors_lab]
+    matrix.append(row)
+
+# Print out a nice table of the similarity matrix
+console = Console(width=420, color_system='truecolor')
+table = Table(title='Color Similarity Matrix', show_header=True)
+table.add_column('')
+for color in chosen_colors_hex:
+    table.add_column(Text(color[1:], style=f'bold {color}'))
+
+for i, row in enumerate(matrix):
+    header = Text(chosen_colors_hex[i], style= f'bold {chosen_colors_hex[i]}')
+    colored_row = [header]
+    for column_value in row:
+        # Here is something I completely made up based on personal observation that won't work for anything else
+        if column_value <= 0:
+            # lime
+            color_value = '#00ff00'
+        elif column_value <= 2:
+            # green
+            color_value = '#008000'
+        elif column_value <= 10:
+            # green yellow
+            color_value = '#afff00'
+        elif column_value <= 27:
+            # orange1
+            color_value = '#ffaf00'
+        elif column_value <= 49:
+            # yellow
+            color_value = '#ffff00'
+        else:
+            # red
+            color_value = '#ff0000'
+        colored_row.append(Text(f'{column_value:.2f}', style=color_value))
+    table.add_row(*colored_row)
+
+
+console.print(table)
